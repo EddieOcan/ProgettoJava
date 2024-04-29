1fd524aa25a3b2deba0b726011aeca476510f82b
lucaavanzii <lucaaavanzi62@gmail.com>
2024-04-29 21:18:59 +0200

    prova

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Worlasdasdd!");
+        System.out.println("Hello, asdasdasd");
     }
 }
